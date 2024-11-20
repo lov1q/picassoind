7c6f5a3c536a4bcbdbf5d2946672b0a6266c04ce
lov1q <n.st1mer@mail.ru>
2024-11-21 02:06:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/picassoind/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.picassoind
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.squareup.picasso.Picasso
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var nextButton : Button
+    private lateinit var imageView : ImageView
+
+    private var currentIndex = 0
+
+    private val imagePaths = listOf(
+        "https://i.pinimg.com/736x/2a/91/b6/2a91b650d14116256d21434b09dbca98.jpg",
+        "https://i.pinimg.com/originals/4e/ca/cb/4ecacbb84ab60c5fe903de1b1d4f5297.jpg",
+        "https://pic.rutubelist.ru/user/4f/0d/4f0dd37df705b4f48bb3895b67a7c826.jpg",
+        "https://i.ytimg.com/vi/eUazGODJJio/maxresdefault.jpg"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nextButton = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % imagePaths.size // Циклический переход по индексам
+            loadImage()
+        }
+    }
+
+    private fun loadImage(){
+        Picasso.get()
+            .load(imagePaths[currentIndex])
+            .error(R.drawable.ic_launcher_background)
+            .resize(250, 250)
+            .into(imageView)
+    }
+}
